473dc506f6383579b86a2765b96f9fe8f8d6a986
jva411 <j.victoralves411@gmail.com>
2023-03-06 18:35:41 -0300

    populating the home component and nade a shadow effect to header

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,5 +1,16 @@
+import { FaUserCircle } from 'react-icons/fa'
+import styles from 'styles/components/home.module.scss'
+
+
 export default function Home() {
-    return <article id='home'>
-        <h1>João Victor Alves</h1>
-    </article>
+    return <section id='home' className={styles.home}>
+        <main>
+            <span>Olá, eu me chamo</span>
+            <h1>João Victor</h1>
+            <FaUserCircle />
+        </main>
+        <article>
+            <p>pensar em um texto</p>
+        </article>
+    </section>
 }
