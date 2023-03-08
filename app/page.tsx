84c4e1fb51351a76fa425018274dea25a2c81703
jva411 <j.victoralves411@gmail.com>
2023-03-08 15:09:13 -0300

    Created the about

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Home from 'components/home'
+import About from 'components/about'
 import type { Metadata } from 'next'
 
 export default function Homepage() {
     return <>
         <Home />
+        <About />
     </>
 }
 
