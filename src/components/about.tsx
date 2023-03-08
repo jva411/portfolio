9b79bbd26eb4890969a626e1f204866825648847
jva411 <j.victoralves411@gmail.com>
2023-03-08 15:51:57 -0300

    some adjusts

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -2,8 +2,8 @@ import styles from 'styles/components/about.module.scss'
 
 export default function About() {
     return <section id='about' className={styles.about}>
-        <div>
-            
+        <div className='centered'>
+
         </div>
     </section>
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,25 +4,27 @@ import styles from 'styles/components/header.module.scss'
 
 export default function Header() {
     return <header className={styles.header}>
-        <a href='https://www.linkedin.com/in/joão-victor-a49192172/' target='_blank' title='Meu Linkedin'>
-            <img src='logo.svg' alt='logo' />
-        </a>
+        <div className='centered'>
+            <a href='https://www.linkedin.com/in/joão-victor-a49192172/' target='_blank' title='Meu Linkedin'>
+                <img src='logo.svg' alt='logo' />
+            </a>
 
-        <nav aria-label='principal'>
-            <ul className={styles.anchors}>
-                <li><a href='#home'>Início</a></li>
-                <li><a href='#about'>Sobre</a></li>
-                <li><a href='#projects'>Projetos</a></li>
-                <li><a href='#footer'>Contato</a></li>
-            </ul>
-        </nav>
+            <nav aria-label='principal'>
+                <ul className={styles.anchors}>
+                    <li><a href='#home'>Início</a></li>
+                    <li><a href='#about'>Sobre</a></li>
+                    <li><a href='#projects'>Projetos</a></li>
+                    <li><a href='#footer'>Contato</a></li>
+                </ul>
+            </nav>
 
-        <nav aria-label='contact'>
-            <ul>
-                <li><a href='https://github.com/jva411' target='_blank' title='Meu GitHub'><BsGithub /></a></li>
-                <li><a href='https://www.linkedin.com/in/joão-victor-a49192172/' target='_blank' title='Meu Linkedin'><BsLinkedin /></a></li>
-                <li><a href='mailto:j.victoralves411@gmail.com' target='_blank' title='Enviar email'><MdOutlineMail /></a></li>
-            </ul>
-        </nav>
+            <nav aria-label='contact'>
+                <ul>
+                    <li><a href='https://github.com/jva411' target='_blank' title='Meu GitHub'><BsGithub /></a></li>
+                    <li><a href='https://www.linkedin.com/in/joão-victor-a49192172/' target='_blank' title='Meu Linkedin'><BsLinkedin /></a></li>
+                    <li><a href='mailto:j.victoralves411@gmail.com' target='_blank' title='Enviar email'><MdOutlineMail /></a></li>
+                </ul>
+            </nav>
+        </div>
     </header>
 }
